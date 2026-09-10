9591f99db655e870e1b370493a0dc3e20d2d53bd
Aduler Viana <adulerviana@gmail.com>
2026-09-10 18:21:02 -0300

    fix: correção do arquivo de conexão

--- src/config/Conexao.php
+++ src/config/Conexao.php
@@ -5,9 +5,9 @@ class Conexao
     public static function getConexao(): PDO
     {
         return new PDO(
-            "mysql:host=127.0.0.1;port=3307;dbname=mydb;charset=utf8mb4",
+            "mysql:host=10.0.0.166;port=3306;dbname=mydb;charset=utf8mb4",
             "root",
-            "",
+            "root#sisgame#mysql",
             [
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
             ]
