bb60d939b7c73cd932a2950043301b2ba3a6b77f
Aduler Viana <adulerviana@gmail.com>
2026-09-10 18:17:05 -0300

    config: adição do arquivo que redireciona ao front controller

--- src/config/Conexao.php
+++ src/config/Conexao.php
@@ -5,12 +5,32 @@ class Conexao
     public static function getConexao(): PDO
     {
         return new PDO(
-            "mysql:host=10.0.0.166;port=3306;dbname=mydb;charset=utf8mb4",
-            "sisgame_user",
-            "user#sisgame#mysql",
+            "mysql:host=127.0.0.1;port=3307;dbname=mydb;charset=utf8mb4",
+            "root",
+            "",
             [
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
             ]
         );
     }
 }
+/*
+
+<?php
+
+class Conexao
+{
+    public static function getConexao(): PDO
+    {
+        return new PDO(
+            "mysql:host=127.0.0.1;port=3307;dbname=mydb;charset=utf8mb4",
+            "root",
+            "",
+            [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+            ]
+        );
+    }
+}
+
+*/
