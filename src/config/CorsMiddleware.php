0af2bd747c48b6e4f24de3b8b06f57905b996c24
Aduler Viana <adulerviana@gmail.com>
2026-09-10 18:43:36 -0300

    fix: testes de retorno

--- src/config/CorsMiddleware.php
+++ src/config/CorsMiddleware.php
@@ -8,15 +8,18 @@ class CorsMiddleware
 {
     public static function aplicar(): void
     {
-        header("Access-Control-Allow-Origin: https://sisgame.jf.ifsudestemg.edu.br/");
-
-        //header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+        // Removida a barra '/' do final da URL
+        header("Access-Control-Allow-Origin: https://sisgame.jf.ifsudestemg.edu.br");
+        
         header("Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE, OPTIONS");
+        header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With, Access-Control-Request-Private-Network");
+        
+        // Permite requisições de rede privada (HTTPS para IP Local)
+        header("Access-Control-Allow-Private-Network: true");
 
-        header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
-
+        // Trata requisições Preflight (OPTIONS)
         if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-            http_response_code(200);
+            http_response_code(204);
             exit;
         }
     }
