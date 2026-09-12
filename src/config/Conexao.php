e5a11aefe26df2181079de51907356fbde710360
Aduler Viana <adulerviana@gmail.com>
2026-09-12 18:16:32 -0300

    fix: correções

--- src/config/Conexao.php
+++ src/config/Conexao.php
@@ -5,7 +5,7 @@ class Conexao
     public static function getConexao(): PDO
     {
         return new PDO(
-            "mysql:host=127.0.0.1;port=3307;dbname=mydb;charset=utf8mb4",
+            "mysql:host=10.0.0.166;port=3306;dbname=mydb;charset=utf8mb4",
             "root",
             "",
             [
